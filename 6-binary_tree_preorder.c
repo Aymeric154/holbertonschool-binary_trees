2d05c8083a00def2fdf29a7c768e044969369be1
Aymeric154 <aymeric.gelle@outlook.fr>
2024-08-05 15:17:00 +0200

    check task which through the binary tree

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - function that goes through
+ *  a binary tree using pre-order traversal
+ *
+ *  @tree:  is a pointer to the root node of the tree to traverse
+ *  @func: is a pointer to a function to call for each node.
+ */
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+
+}
