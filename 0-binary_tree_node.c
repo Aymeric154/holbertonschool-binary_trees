c587bb83dda51c91e2151a1327f9d8ea1c13e451
Aymeric154 <aymeric.gelle@outlook.fr>
2024-08-05 12:40:37 +0200

    add condition if new node is null

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -15,6 +15,9 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
 
+	if (newNode == NULL)
+		return (NULL);
+
 	newNode->parent = parent;
 	newNode->n = value;
 	newNode->left = NULL;
