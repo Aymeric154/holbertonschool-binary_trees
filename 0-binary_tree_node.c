aa97c14b77eb9a789aa13f023dac6aa90482eff6
Aymeric154 <aymeric.gelle@outlook.fr>
2024-08-05 12:33:11 +0200

    first commit to create new node in binary tree

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * *binary_tree_node -  function that creates a binary tree node
+ *
+ * @parent: is a pointer to the parent node of the node to create
+ * @value: is the value to put in the new node
+ *
+ * Return: pointer to the new node, or NULL on failure
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+
+	binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
+
+	newNode->parent = parent;
+	newNode->n = value;
+	newNode->left = NULL;
+	newNode->right = NULL;
+
+	return (newNode);
+}
