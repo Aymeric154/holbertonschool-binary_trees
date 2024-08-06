40db22ffa4fabbda35611a5fce952ea14afed3b2
Aymeric154 <aymeric.gelle@outlook.fr>
2024-08-06 14:39:37 +0200

    fix and clean code

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -12,18 +12,17 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	binary_tree_t *grand_parent = node->parent->parent;
+	binary_tree_t *grand_parent;
 
-	if (node == NULL || node->parent == NULL ||
-			grand_parent == NULL)
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
+
+	grand_parent = node->parent->parent;
 
 	if (grand_parent->left == node->parent)
 		return (grand_parent->right);
 
 	else if (grand_parent->right == node->parent)
 		return (grand_parent->left);
-
-	return (0);
 
 }
