3be28f5c8f32740b2f2495e3d10605b7597632b9
Thomas Maye <thomas.maye.33@gmail.com>
2024-08-06 09:22:18 +0200

    Correct this code with an additionnal condition

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -16,10 +16,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 
-	if (left_height > right_height)
+	if (left_height >= right_height)
 		return (left_height + 1);
 	else
 		return (right_height + 1);
